58130c9b60558173f8c380ff28b7391f0bc1f4cb
김연길 <@121.175.81.109>
2020-11-19 22:13:23 +0900

    [20201119] 수주처리 김연길

---
+++ saleforce/src/main/java/com/project/saleforce/repository/OrderDao.java
@@ -0,0 +1,59 @@
+package com.project.saleforce.repository;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.project.saleforce.model.OrderVO;
+
+@Repository
+public class OrderDao {
+	
+	@Autowired
+	SqlSession sql;
+	
+	public List<OrderVO> getOrderList(int start, int end, String searchOpt,String words) {
+		HashMap<String, Object> hs = new HashMap<String, Object>();
+		hs.put("searchOpt", searchOpt);
+		hs.put("words", words);
+		hs.put("start", start);
+		hs.put("end", end);
+		
+		return sql.selectList("order.getOrderList", hs);
+	}
+	
+	public int getOrderCount(String searchOpt,String words) {
+		HashMap<String, String> hs = new HashMap<String, String>();
+		hs.put("searchOpt", searchOpt);
+		hs.put("words", words);
+		
+		return sql.selectOne("order.getOrderCount", hs);
+	}
+	
+	public OrderVO getOneOrder(String ordnum) {
+		return sql.selectOne("order.getOneOrder", ordnum);
+	}
+	
+	public OrderVO getItemInf(String itemcd) {
+		return sql.selectOne("order.getItemInf", itemcd);
+	}
+	
+	public String getCompanynm(String comcd) {
+		return sql.selectOne("order.getCompanynm", comcd);
+	}
+	
+	public void setOrder(OrderVO ovo) {
+		sql.insert("order.setOrder", ovo);
+	}	
+	
+	public void updateOrderInfo(OrderVO ovo) {
+		sql.update("order.updateOrderInfo", ovo);
+	}
+	
+	public void deleteOrderInfo(String ordnum) {
+		sql.delete("order.deleteOrderInfo", ordnum);
+	}
+}
