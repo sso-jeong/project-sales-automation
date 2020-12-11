1f1274fbb2a284bc79a51e5aa793759a89cb65d4
user <user@BS_CW12>
2020-12-11 10:07:11 +0900

    20201210 김연길 판매완료, 재고 팝업창 남음

--- saleforce/src/main/java/com/project/saleforce/repository/EmpDao.java
+++ saleforce/src/main/java/com/project/saleforce/repository/EmpDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 
 import com.project.saleforce.model.EmpVO;
